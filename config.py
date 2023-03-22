ee0e1f8aa13c835f38515cb8096b7917a6ed6e41
theRate <adskiypovar@gmail.com>
2023-03-22 23:11:45 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "5706807992:AAH0rah9wE2krHdosA2IgfkpudTCFk2kSlQ"
+
+cur = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+
+headers = {
+    "apikey": "KkdhtM2y7GcBzKdvRGjcjP2paEeSFhJI"
+}
